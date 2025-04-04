0d8d66cc844b3bab214a1749f0a96fcb5af98b89
[name] <nehapg2001@gmail.com>
2025-04-05 01:46:05 +0530

    Adding files of material-component

---
+++ Petpooja-Frontend/src/app/material-component/dialog/product/product.component.ts
@@ -0,0 +1,134 @@
+import { Component, EventEmitter, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { CategoryService } from 'src/app/services/category.service';
+import { ProductService } from 'src/app/services/product.service';
+import { SnackbarService } from 'src/app/services/snackbar.service';
+import { GlobalConstants } from 'src/app/shared/global-constant';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent implements OnInit {
+  onAddProduct = new EventEmitter();
+  onEditProduct = new EventEmitter();
+  productForm: any = FormGroup;
+  dialogAction: any = 'Add';
+  action: any = 'Add';
+  responseMessage: any;
+  categorys: any = [];
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public dialogData: any,
+    private formBuilder: FormBuilder,
+    private productService: ProductService,
+    public dialogRef: MatDialogRef<ProductComponent>,
+    private snackbarService: SnackbarService,
+    private categoryService: CategoryService
+  ) { }
+
+  ngOnInit(): void {
+    this.productForm = this.formBuilder.group({
+      name: [null, [Validators.required, Validators.pattern(GlobalConstants.nameRegex)]],
+      categoryId: [null, [Validators.required]],
+      description: [null, [Validators.required]],
+      price: [null, [Validators.required]]
+    });
+    if (this.dialogData.action === 'Edit') {
+      this.dialogAction = 'Edit';
+      this.action = 'Update';
+      this.productForm.patchValue(this.dialogData.data);
+    }
+    this.getCategorys();
+  }
+
+  getCategorys() {
+    this.categoryService.getCategorys().subscribe(
+      (response: any) => {
+        this.categorys = response;
+      },
+      (error: any) => {
+        if (error.error?.message) {
+          this.responseMessage = error.error?.message;
+        } else {
+          this.responseMessage = GlobalConstants.genericError;
+        }
+        this.snackbarService.openSnackBar(
+          this.responseMessage,
+          GlobalConstants.error
+        );
+      }
+    );
+  }
+
+  handleSubmit() {
+    if (this.dialogAction === 'Edit') {
+      this.edit();
+    } else {
+      this.add();
+    }
+  }
+
+  add() {
+    var formdata = this.productForm.value;
+    var data = {
+      name: formdata.name,
+      categoryId: formdata.categoryId,
+      description: formdata.description,
+      price: formdata.price
+    };
+    this.productService.add(data).subscribe(
+      (response: any) => {
+        this.dialogRef.close();
+        this.onAddProduct.emit();
+        this.responseMessage = response.message;
+        this.snackbarService.openSnackBar(this.responseMessage, 'success');
+      },
+      (error: any) => {
+        // this.dialogRef.close();
+        if (error.error?.message) {
+          this.responseMessage = error.error?.message;
+        } else {
+          this.responseMessage = GlobalConstants.genericError;
+        }
+        this.snackbarService.openSnackBar(
+          this.responseMessage,
+          GlobalConstants.error
+        );
+      }
+    );
+  }
+
+  edit() {
+    var formdata = this.productForm.value;
+    var data = {
+      id: this.dialogData.data.id,
+      name: formdata.name,
+      categoryId: formdata.categoryId,
+      description: formdata.description,
+      price: formdata.price
+    };
+    this.productService.update(data).subscribe(
+      (response: any) => {
+        this.dialogRef.close();
+        this.onEditProduct.emit();
+        this.responseMessage = response.message;
+        this.snackbarService.openSnackBar(this.responseMessage, 'success');
+      },
+      (error: any) => {
+        // this.dialogRef.close();
+        if (error.error?.message) {
+          this.responseMessage = error.error?.message;
+        } else {
+          this.responseMessage = GlobalConstants.genericError;
+        }
+        this.snackbarService.openSnackBar(
+          this.responseMessage,
+          GlobalConstants.error
+        );
+      }
+    );
+  }
+}
---
+++ Petpooja-Frontend/src/app/material-component/material.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { CdkTableModule } from '@angular/cdk/table';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MaterialRoutes } from './material.routing';
+import { MaterialModule } from '../shared/material-module';
+import { ViewBillProductsComponent } from './dialog/view-bill-products/view-bill-products.component';
+import { ConfirmationComponent } from './dialog/confirmation/confirmation.component';
+import { ChangePasswordComponent } from './dialog/change-password/change-password.component';
+// import { ManageComponentComponent } from './manage-component/manage-component.component';
+import { ManageCategoryComponent } from './manage-category/manage-category.component';
+import { CategoryComponent } from './dialog/category/category.component';
+import { ManageProductComponent } from './manage-product/manage-product.component';
+import { ProductComponent } from './dialog/product/product.component';
+import { ManageUserComponent } from './manage-user/manage-user.component';
+import { ManageOrderComponent } from './manage-order/manage-order.component';
+// import { ManageBillComponent } from './manage-bill/manage-bill.component';
+import { ViewBillComponent } from './view-bill/view-bill.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(MaterialRoutes),
+    MaterialModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    CdkTableModule
+  ],
+  providers: [],
+  declarations: [
+    ViewBillProductsComponent,
+    ConfirmationComponent,
+    ChangePasswordComponent,
+    // ManageComponentComponent,
+    ManageCategoryComponent,
+    CategoryComponent,
+    ManageProductComponent,
+    ProductComponent,
+    ManageUserComponent,
+    ManageOrderComponent,
+    ViewBillComponent    
+  ]
+})
+export class MaterialComponentsModule {}
